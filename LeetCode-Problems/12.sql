81512a36a6dc5f0e7e4e190bf3b62b311a960234
Jerald jenid J <jeraldjenid17@gmail.com>
2026-03-09 19:49:39 +0530

    Create 12.sql

---
+++ LeetCode-Problems/12.sql
@@ -0,0 +1,12 @@
+-- Sales Person
+
+
+select name
+from salesperson
+where sales_id not in(
+    select sales_id
+    from orders
+    join company on orders.com_id=
+    company.com_id  
+    where company.name='Red')
+;
